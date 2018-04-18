81e45a5dd06c5b5eda3b1b2ec01f46e315d1feda
Andreas Weise <a.weise@avm.de>
2018-04-18 17:45:38 +0200

    add Elytron example

---
+++ security/elytron/basic/src/main/java/org/wildfly/swarm/examples/elytron/basic/EmployeeResource.java
@@ -0,0 +1,31 @@
+package org.wildfly.swarm.examples.elytron.basic;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ * @author Ken Finnigan
+ */
+@Path("/")
+@ApplicationScoped
+public class EmployeeResource {
+
+    @PersistenceContext
+    EntityManager em;
+    @Context 
+    SecurityContext securityContext;
+
+    @GET
+    @Produces("application/json")
+    public Employee[] get() {
+        return securityContext.isUserInRole("admin")
+            ? em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0])
+            : new Employee[0];
+    }
+}
